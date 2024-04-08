9111fea3c4a22691e5f3a2aabb8a7a66cfcb96a3
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-04-07 22:05:49 -0300

    First commit

---
+++ Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace likes_exercise.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
